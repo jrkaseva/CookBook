26d575a12aa15f6dbc26a59869d887a328b096a3
jrkaseva <joakim.r.kaseva@student.jyu.fi>
2023-06-07 15:22:24 +0300

    FXML files
    Still undone:  popup dialogs. Also unrelated to phase2 - java-files created

---
+++ src/application/HomeController.java
@@ -0,0 +1,15 @@
+package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * @author tietokone
+ * @version 7.6.2023
+ *
+ */
+public class HomeController{
+
+    @FXML
+    private Button btnHome;
+
+}
---
+++ src/application/MainController.java
@@ -0,0 +1,102 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+/**
+ * @author tietokone
+ * @version 7.6.2023
+ * Controller for main scene
+ */
+public class MainController{
+
+    @FXML
+    private MenuItem menuAddR;
+
+    @FXML
+    private MenuItem menuDelR;
+
+    @FXML
+    private Menu menuEdit;
+
+    @FXML
+    private MenuItem menuFavR;
+
+    @FXML
+    private Menu menuFile;
+
+    @FXML
+    private Menu menuHelp;
+
+    @FXML
+    private MenuItem menuInfo;
+
+    @FXML
+    private MenuItem menuModR;
+
+    @FXML
+    private MenuItem menuOpen;
+
+    @FXML
+    private MenuItem menuPrint;
+
+    @FXML
+    private MenuItem menuQuit;
+
+    @FXML
+    private MenuItem menuSave;
+
+    @FXML
+    private MenuItem menuShowHelp;
+
+    @FXML
+    private MenuBar menuValikko;
+
+    @FXML
+    void etsiMenu(ActionEvent event) {
+        // 
+    }
+
+    @FXML
+    void etusivu(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void help(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void info(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void lisaaMenu(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void lopetaMenu(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void muokkaaMenu(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void poistaMenu(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void tulostaMenu(ActionEvent event) {
+        //
+    }
+
+}
