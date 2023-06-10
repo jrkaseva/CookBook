852948b97b008566820484b93e7199b36734dfd6
jrkaseva <joakim.r.kaseva@student.jyu.fi>
2023-06-10 15:32:09 +0300

    shenanigans

--- src/lib/Storage.java
+++ src/lib/Storage.java
@@ -3,6 +3,9 @@
  */
 package lib;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -43,13 +46,36 @@ public class Storage {
         recipes.remove(index);
     }
     
+    /**
+     * 
+     */
     public void loadData() {
-        sc = new Scanner(RECIPES_FILE);
+        File f = new File(RECIPES_FILE);
+        if (!f.exists())
+            try {
+                f.createNewFile();
+                System.out.printf("File created at: %s\n", f.getAbsoluteFile());
+                return;
+            } catch (IOException e) {
+                System.out.println("Error creating new file");
+                e.printStackTrace();
+            }
+        try {
+            sc = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error with \"new Scanner(File f)\"");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Loading recipes from file...");
+        int count = 0;
         while (sc.hasNextLine()) {
+            count++;
             String line = sc.nextLine();
             System.out.println(line);
             // TODO: get data from .dat-files and if necessary, create .dat-files.
         }
+        System.out.printf("Loaded %d lines\n", count);
     }
     
     public void saveData() {
