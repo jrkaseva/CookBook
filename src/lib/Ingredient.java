89fb723d165bb1a5a303f6487f0c5452ab2b2ac2
jrkaseva <joakim.r.kaseva@student.jyu.fi>
2023-06-12 10:43:32 +0300

    Cleaning

--- src/lib/Ingredient.java
+++ src/lib/Ingredient.java
@@ -11,6 +11,17 @@ package lib;
 public class Ingredient {
     private int id;
     private String name;
+    
+    
+    /**
+     * @param id of Ingredient
+     * @param name of Ingredient
+     */
+    public Ingredient(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
     /**
      * @return the id
      */
@@ -27,5 +38,21 @@ public class Ingredient {
     @Override
     public String toString() {
         return id + " | " + name;
+    }
+    
+    /**
+     * @param s String to be parsed
+     * @return new Ingredient
+     * @throws ParseException if error with parsing
+     */
+    public static Ingredient parse(String s) throws ParseException {
+        String[] arr = s.split("|");
+        if (arr.length != 2) throw new ParseException("Virhe ainesosien lukemisessa: Enemmän kuin yksi '|' rivissä");
+        try {
+            Integer.parseInt(arr[0].trim());
+        } catch (Exception e) {
+            throw new ParseException("Virhe ainesosien lukemisessa: ID ei ole laillinen numero");
+        }
+        return new Ingredient(Integer.parseInt(arr[0].trim()), arr[1].trim());
     }
 }
