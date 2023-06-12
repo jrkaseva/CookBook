921015d706aa6317d8863a498955889c5c163786
jrkaseva <joakim.r.kaseva@student.jyu.fi>
2023-06-12 10:17:51 +0300

    Cleaning

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 
@@ -14,6 +13,9 @@ import javafx.fxml.FXMLLoader;
  *
  */
 public class Main extends Application {
+    /**
+     * Stage o
+     */
     public static Stage stage;
 	@Override
 	public void start(Stage primaryStage) {
